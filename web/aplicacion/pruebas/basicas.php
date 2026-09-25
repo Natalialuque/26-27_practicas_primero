3f25e9cd3c22025ca9506186b19f8388a9e35017
natalia <tuncabluq878@g.educaand.es>
2026-09-25 14:55:38 +0200

    nuevo proyecto con algunos cambios

--- web/aplicacion/pruebas/basicas.php
+++ web/aplicacion/pruebas/basicas.php
@@ -1,28 +1,22 @@
 <?php
-include_once(dirname(__FILE__) . "/cabecera.php");
+include_once(dirname(__FILE__) . "/../../cabecera.php");
 //controlador
-
-$usuario=getenv("MYSQL_USER");
-
 //dibuja la plantilla de la vista
 inicioCabecera("APLICACION PRIMER TRIMESTRE");
 cabecera();
 finCabecera();
 inicioCuerpo("2DAW APLICACION");
-cuerpo();  //llamo a la vista
+cuerpo(); //llamo a la vista
 finCuerpo();
 // **********************************************************
-
 //vista
-function cabecera() 
+function cabecera()
 {}
-
 //vista
 function cuerpo()
 {
 ?>
-    <br><br>
-   
-    <a href="./aplicacion/pruebas/index.php">Acceso a pruebas</a>
+<br><br>
+cristian gay
 <?php
 }
--- web/aplicacion/pruebas/index.php
+++ web/aplicacion/pruebas/index.php
@@ -1,28 +1,24 @@
 <?php
-include_once(dirname(__FILE__) . "/cabecera.php");
+include_once(dirname(__FILE__) . "/../../cabecera.php");
 //controlador
-
-$usuario=getenv("MYSQL_USER");
-
 //dibuja la plantilla de la vista
 inicioCabecera("APLICACION PRIMER TRIMESTRE");
 cabecera();
 finCabecera();
 inicioCuerpo("2DAW APLICACION");
-cuerpo();  //llamo a la vista
+cuerpo(); //llamo a la vista
 finCuerpo();
 // **********************************************************
-
 //vista
-function cabecera() 
-{}
-
+function cabecera() {}
 //vista
 function cuerpo()
 {
 ?>
     <br><br>
-   
-    <a href="./aplicacion/pruebas/index.php">Acceso a pruebas</a>
+    buenos dias pequeño saltamontes 
+    <br><br>
+    <a href="basicas.php">funcionamiento</a>
+
 <?php
 }
